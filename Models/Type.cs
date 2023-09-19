cde1ddb0c0d78b6caec4968970101f6c441c8247
ariavalenci <aria@valenci.no>
2023-09-19 17:58:23 +0200

    add model for species type

---
+++ Models/Type.cs
@@ -0,0 +1,9 @@
+namespace NytteveksterApi.Models;
+
+public class Type
+{
+  public int Id { get; set; }
+  public required string Name { get; set; }
+  public string? Description { get; set; }
+  public required string ImagePath { get; set; }
+}

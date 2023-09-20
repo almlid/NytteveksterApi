cd930c9e50fb7ff5ad3330dd1c8e1c2d2d875e8e
ariavalenci <aria@valenci.no>
2023-09-20 16:21:59 +0200

    configure API versioning, database and CORS

--- Program.cs
+++ Program.cs
@@ -1,19 +1,41 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using NytteveksterApi.Contexts;
+
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
+builder.Services.AddApiVersioning(options =>
+  {
+    options.DefaultApiVersion = new ApiVersion(1, 0);
+    options.AssumeDefaultVersionWhenUnspecified = true;
+    options.ReportApiVersions = true;
+  });
+
+builder.Services.AddDbContext<NytteveksterContext>(options => options.UseSqlite("Data Source=nyttevekster.db"));
+builder.Services.AddCors(
+options =>
+{
+  options.AddPolicy("AllowAll",
+    builder => builder
+    .AllowAnyHeader()
+    .AllowAnyMethod()
+    .AllowAnyOrigin()
+  );
+}
+);
 
 builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
+app.UseCors("AllowAll");
+
 if (app.Environment.IsDevelopment())
 {
-    app.UseSwagger();
-    app.UseSwaggerUI();
+  app.UseSwagger();
+  app.UseSwaggerUI();
 }
 
 app.UseHttpsRedirection();

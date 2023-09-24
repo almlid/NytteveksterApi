6ed5b13ed246d5f1a39df44f212452d3164a3dc6
ariavalenci <aria@valenci.no>
2023-09-24 18:21:11 +0200

    configure controller and base get endpoint for months

---
+++ Controllers/MonthsController.cs
@@ -0,0 +1,70 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using NytteveksterApi.Contexts;
+using NytteveksterApi.Dtos;
+
+namespace NytteveksterApi.Controllers;
+
+[ApiController]
+[Route("api/v{version:apiVersion}/[controller]")]
+[ApiVersion("1.0")]
+public class MonthsController : ControllerBase
+{
+  private readonly NytteveksterContext context;
+  private readonly IHttpContextAccessor httpContextAccessor;
+  private readonly string apiVersion;
+
+  public MonthsController(NytteveksterContext _context, IHttpContextAccessor _httpContextAccessor)
+  {
+    context = _context;
+    httpContextAccessor = _httpContextAccessor;
+    apiVersion = _httpContextAccessor.HttpContext?.GetRequestedApiVersion()?.ToString() ?? "1.0";
+  }
+
+  [HttpGet]
+  public async Task<ActionResult<ListResponseDTO<MonthlyAvailabilityDto>>> GetMonths()
+  {
+    try
+    {
+      var allSpeciesAvailability = await context.SpeciesAvailability.ToListAsync();
+      if (allSpeciesAvailability == null)
+      {
+        return NotFound();
+      }
+
+      ICollection<string> Months = new List<string> { "January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December" };
+      List<MonthlyAvailabilityDto> monthDtoList = new List<MonthlyAvailabilityDto>();
+
+      foreach (var month in Months)
+      {
+        var monthDto = new MonthlyAvailabilityDto
+        {
+          Month = month,
+          SpeciesCount = allSpeciesAvailability
+                .Count(sa => (bool)sa
+                  .GetType()
+                  .GetProperty(month)
+                  .GetValue(sa)),
+          SpeciesUrl = $"/api/v{apiVersion}/species/months/{month}"
+        };
+
+        monthDtoList.Add(monthDto);
+      }
+
+      var dto = new ListResponseDTO<MonthlyAvailabilityDto>
+      {
+        Count = monthDtoList.Count,
+        Results = monthDtoList
+      };
+
+      return Ok(dto);
+
+    }
+    catch (Exception e)
+    {
+      return StatusCode(500, e.Message);
+    }
+  }
+
+}
+
---
+++ Dtos/MonthlyAvailabilityDto.cs
@@ -0,0 +1,8 @@
+namespace NytteveksterApi.Dtos;
+
+public class MonthlyAvailabilityDto
+{
+  public string Month { get; set; }
+  public int SpeciesCount { get; set; }
+  public string SpeciesUrl { get; set; }
+}
